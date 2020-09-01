537741a56645ce2be9b9b49bf87046fcd6ac182d
Doug Hughes <dhughes8906@gmail.com>
2020-09-01 12:40:12 -0400

    Takes out previous testing code on adding items that is no longer neccesary

--- src/index.js
+++ src/index.js
@@ -21,23 +21,6 @@ setupPage();
 // get the home project
 let homeProject = userProfile.getProject(userProfile.getHomeIndex());
 
-// add a test item to the project
-homeProject.addItem();
-
-let testItem = homeProject.getItem(0);
-
-testItem.changeTitle("Test");
-testItem.changeDesc("Hi this is a test");
-testItem.changeDueDate("Friday");
-testItem.changePriority(6);
-
-// add another test item
-homeProject.addItem();
-
-let secItem = homeProject.getItem(1);
-
-secItem.changeTitle("Second Item");
-
 // add the home project to the page
 displayProject(homeProject);
 
