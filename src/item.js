dd7c4953117245deec37341f86ac52e62c2ff448
Doug Hughes <dhughes8906@gmail.com>
2020-09-03 21:31:34 -0400

    Changes priority for an item to be a string rather than a number

--- src/item.js
+++ src/item.js
@@ -3,15 +3,11 @@ const Item = (creationOrder) => {
 	let title = 'New item';
 	let description = '';
 	let dueDate = '';	
-	let priority = 1;
+	let priority = '';
 	let complete = false;
 	// for use by a project object to track the order in which this
 	// item was created among all items in the project
-	let order = creationOrder;
-
-	// minimum and maximum priorities for any item
-	const minPriority = 1;
-	const maxPriority = 10;
+	let order = creationOrder;	
 
 	const changeTitle = (newTitle) => {
 		title = newTitle;
@@ -26,32 +22,7 @@ const Item = (creationOrder) => {
 	}
 
 	const changePriority = (newPriority) => {
-		if (typeof newPriority === 'number') {
-			let remainder = newPriority % 1;
-			if (remainder === 0) {
-				if (newPriority < minPriority) {
-					console.log(`Entered priority of ${newPriority} is below the minimum priority of ${minPriority}`);
-					newPriority = minPriority;
-				}
-				else if (newPriority > maxPriority) {
-					console.log(`Entered priority of ${newPriority} is above the maximum priority of ${maxPriority}`);
-					newPriority = maxPriority;
-				}
-				priority = newPriority;	
-				console.log(`Priority for the ${title} task changed to ${priority}.`);
-			}
-			// input is NaN, infinity or -infinity
-			else if (isNaN(remainder)) {
-				console.log(`NaN, Infinity or -Infinity was entered. Priority for the ${title} task unchanged.`);
-			}
-			else {
-				console.log(`A floating number was not entered. Priority for the ${title} task unchanged.`);	
-			}
-		}
-		// input is not a number
-		else {
-			console.log(`A number was not entered. Priority for the ${title} task unchanged.`);
-		}
+		priority = newPriority;	
 	}
 
 	const setComplete = () => {
